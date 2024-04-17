76ffc6cdf0a3fcc9012f91e77ea145f4129710ad
Cesar Guerrero <cguerrero@Cesars-MacBook-Pro.local>
2024-04-17 15:09:22 -0700

    GraphQL setup completed. Now need to work on resolver

--- src/index.ts
+++ src/index.ts
@@ -9,20 +9,44 @@ import express, { Response, Request } from 'express';
 
 import createApp from './app';
 
+import { buildSchema } from 'type-graphql';
+import { createHandler } from 'graphql-http';
+import { ruruHTML } from 'ruru/server';
+
+//Resolvers
+import AppetizerResolver from './resolvers/appetizer-resolver';
+
 async function main(){
-    /**
-     * Handling building the graphql schema here
-     */
+
+    const schema = await buildSchema({
+        resolvers: [AppetizerResolver],
+        emitSchemaFile: true //Creates schema.graphql file in current directory
+    });
+
+    if(!schema){
+        console.log("Error creating schema");
+        process.exit(1);
+    }
 
     //We don't provide any argument which means this will be a development environment
     const app = await createApp();
 
-    /**
-     * Handling the graphql queries here
-     */
+    //Handle graphql requests - In Porduction this should be POST
+    app.all('/graphql', (req: Request, res: Response) => {
+        createHandler({
+            schema: schema
+        })
+    });
+
+    //We want to deploy a GraphiQL interface for development purposes
+    app.get("/", (req: Request, res: Response) => {
+        res.type("html")
+        res.end(ruruHTML({ endpoint: "/graphql" }))
+    });
 
     //Server listens on port 4000
     app.listen(4000);
+    console.log("Running a GraphQL API server at http://localhost:4000/graphql");
 }
 
 //Start the server
---
+++ src/resolvers/appetizer-resolver.ts
@@ -0,0 +1,17 @@
+/**
+ * Cesar Guerrero
+ * 04/17/24
+ * 
+ * @file The query and mutation resolver for the appetizer table
+ */
+
+
+import AppetizerDAO from "../daos/appetizer-dao";
+import { Appetizer } from "../models/appetizer";
+
+/**
+ * The query and mutation resolver for the appetizer table
+ */
+export default class AppetizerResolver{
+
+};
